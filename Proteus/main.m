9f5266097d340cc63d1c504ce9e2fd94364c201d
David Owens II <owensd@kiadsoftware.com>
2014-10-01 17:01:37 -0700

    Initial Commit

---
+++ Proteus/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Proteus
+//
+//  Created by David Owens II on 10/1/14.
+//  Copyright (c) 2014 owensd.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
